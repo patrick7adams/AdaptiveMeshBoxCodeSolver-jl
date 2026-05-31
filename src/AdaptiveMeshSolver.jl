9f6aff04f481b29747df271eee51ac53476bdf1d
Patrick Adams <patrick4adams@gmail.com>
2026-05-31 05:58:03 -1000

    Built into package for testing

---
+++ src/AdaptiveMeshSolver.jl
@@ -0,0 +1,18 @@
+module AdaptiveMeshSolver
+
+using P4estTypes;
+using Polynomials
+using FastChebInterp
+using Inti, Meshes, CairoMakie, StaticArrays, Gmsh
+using PolygonAlgorithms
+using GeometryBasics
+using Colorfy
+using FMM2D
+using IterativeSolvers, LinearAlgebra
+using Revise
+
+include("mesh_tools.jl")
+
+include("main.jl")
+
+end
